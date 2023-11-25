f3251dba1938283e68af1a9c8e50afe312083d13
Sebastien Chahoud <sebastien.chahoud@gmail.com>
2023-11-25 18:46:02 +0800

    start state estimation src, preproprecissing done

---
+++ src/state_estimation/config/vehicle_params.py
@@ -0,0 +1,42 @@
+
+class VehicleParams:
+
+    # Aero
+    A_front = 1.61
+    c_drag = 0.97
+    c_lift = 2.33
+    rho_air = 1.2
+
+    # Motor
+    w_m_max = 681
+    tau_m_max = 21
+    gear_ratio = 13.188
+    tau_w_max = 129
+
+    # Steering
+    kin = 0.233
+    kout = 0.162
+
+    # Rigid parameters
+    g = 0.81
+    lzz = 180
+    a = 1.24
+    b = 1.24
+    l = 1.57
+    lf = 0.785
+    lr = 0.785
+    m_car = 300
+    z_cg = 0.288
+
+    # Wheel parameters
+    lw = 0.3
+    Rw = 0.2
+    kb = 0.18
+    kd = 0.17
+    ks = 15
+
+    # Tire parameters
+    B = 11.15
+    C = 1.98
+    D = 1.67
+    E = 0.97
---
+++ src/state_estimation/inputs/read_inputs.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from typing import Tuple, TypedDict, List, Sequence
+
+
+class Inputs(TypedDict):
+    ins: Sequence[float]
+    bps: Sequence[float]
+    torques: Sequence[float]
+    motor_speeds: Sequence[int]
+    steering_angle: float
+
+
+def get_inputs(inputs: pd.DataFrame) -> Inputs:
+    INS_index = [0, 1, 2]
+    BP_index = [3, 4]
+    TorqueFeedBack_index = [5, 6, 7, 8]
+    MotorSpeeds_index = [9, 10, 11, 12]
+    angle_steering_index = 13
+    ins = inputs[INS_index]
+    bps = inputs[BP_index]
+    torques = inputs[TorqueFeedBack_index]
+    motor_speeds = inputs[MotorSpeeds_index]
+    steering_angle = inputs[angle_steering_index]
+    return dict(
+        ins=ins,
+        bps=bps,
+        torques=torques,
+        motor_speeds=motor_speeds,
+        steering_angle=steering_angle
+    )
