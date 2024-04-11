24fb70b0f3b14d4c4930227c1aeda7a465330f72
chahoud <sebastien.chahoud@epfl.ch>
2024-04-12 00:05:17 +0200

    telemtry description tab

--- src/backend/data_crud/json_crud.py
+++ src/backend/data_crud/json_crud.py
@@ -1,6 +1,7 @@
 from typing import TypedDict
 import json
 from src.backend.data_crud.base import CRUD
+from copy import deepcopy
 
 
 class TelemetryDescription(TypedDict):
@@ -13,43 +14,59 @@ class JsonCRUD(CRUD):
     data: dict[str, dict[str]]
 
     def __init__(self, file_path_name: str):
-        assert file_path_name[:5] == '.json', "File must be of type 'json'"
+        assert file_path_name.endswith('.json'), "File must be of type 'json'"
         self.file_path_name = file_path_name
         with open(file_path_name, 'r') as f:
-            self.data = json.load(f)
+            if f.read() == "":
+                self.data = {}
+            else:
+                f.seek(0)
+                self.data = json.load(f)
 
-    def _get_data(self, bucket_name: str, field_name: str) -> TelemetryDescription | None:
-        bucket_res = self.data.get(bucket_name, None)
-        if bucket_res:
-            return bucket_res.get(field_name, None)
+    @classmethod
+    def _get_keys(cls, field_name: str) -> tuple[str, str]:
+        splits = field_name.split('_')
+        return splits[0], '_'.join(splits[1:])
+
+    def _get_data(self, keys: tuple[str, str]) -> TelemetryDescription | None:
+        assert len(keys) == 2
+        res = self.data.get(keys[0], None)
+        if res:
+            return res.get(keys[1], None)
         else:
             return None
 
-    def _set_data(self, bucket_name: str, field_name: str, unit: str, description: str):
-        new_data: TelemetryDescription = dict(unit=unit, description=description)
-        if bucket_name not in self.data:
-            self.data[bucket_name] = {}
-        self.data[bucket_name][field_name] = new_data
+    def _set_data(self, keys: tuple[str, str], unit: str, description: str):
+        new_data = dict(unit=unit, description=description)
+        if keys[0] not in self.data:
+            self.data[keys[0]] = {}
+        self.data[keys[0]][keys[1]] = new_data
 
-    def create(self, bucket_name: str, field_name: str, unit: str, description: str) -> bool:
-        self._set_data(bucket_name, field_name, unit, description)
+    def create(self, field_name: str, unit: str, description: str) -> bool:
+        self._set_data(self._get_keys(field_name), unit, description)
         with open(self.file_path_name, 'w') as f:
-            json.dumps(f, sort_keys=True, indent=4)
+            f.write(json.dumps(self.data, sort_keys=True, indent=4))
         return True
 
-    def update(self, bucket_name: str, field_name: str, unit: str, description: str) -> bool:
-        return self.create(bucket_name, field_name, unit, description)
+    def update(self, field_name: str, unit: str, description: str) -> bool:
+        return self.create(field_name, unit, description)
 
     def read(self, bucket_name: str, field_name: str) -> TelemetryDescription:
-        data_read = self._get_data(bucket_name, field_name)
+        data_read = self._get_data(self._get_keys(field_name))
         if data_read:
             return data_read
         else:
-            return dict(unit="None", description="None")
+            return dict(unit=None, description=None)
 
-    def delete(self, bucket_name: str, field_name: str = None) -> bool:
-        assert bucket_name in self.data
-        del self.data[bucket_name][field_name]
-        if field_name == None:
-            del self.data[bucket_name]
-        return True
+    def delete(self, field_name: str) -> bool:
+        keys = self._get_keys(field_name)
+        if keys[0] in self.data:
+            if keys[1] in self.data[keys[0]]:
+                del self.data[keys[0]][keys[1]]
+                with open(self.file_path_name, 'w') as f:
+                    json.dumps(f, sort_keys=True, indent=4)
+                return True
+        return False
+
+    def get_all(self) -> dict[str, dict[str]]:
+        return deepcopy(self.data)
---
+++ src/frontend/tabs/telemetry_description_tab.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import streamlit as st
+from src.backend.sessions.create_sessions import SessionCreator
+from src.backend.data_crud.json_crud import JsonCRUD
+from src.frontend.tabs.base import Tab
+
+
+class TelemetryDescriptionTab(Tab):
+    telemetry_description_file_path = "data/telemetry_description.json"
+    bucket_data_names = ["AMS", "MISC", "VSI", "sensors"]
+    units = list(sorted(["m/s", "rad", "°", "rad/s", "m", "m/s²", "A", "mA", "rpm", "I", "U", "None"]))
+
+    def __init__(self):
+        super().__init__("telemetry_description_tab", "Telemetry Description")
+
+        if "data" not in self.memory:
+            self.memory['data'] = pd.DataFrame()
+
+        if "telemetry_description" not in self.memory:
+            self.memory['telemetry_description_crud'] = JsonCRUD(self.telemetry_description_file_path)
+
+    def build(self, session_creator: SessionCreator):
+
+        st.header(self.description)
+        datetime_range = session_creator.r2d_session_selector(st.session_state.sessions,
+                                                              key=f"{self.name} session selector")
+        if st.button("Fetch this session", key=f"{self.name} fetch data button"):
+            data = session_creator.fetch_data(datetime_range, verify_ssl=st.session_state.verify_ssl)
+            data.index = (data.index - data.index[0]).total_seconds()
+            self.memory['data'] = data
+
+        if len(self.memory['data']) > 0:
+            data_bucket = st.selectbox("Select the data bucket", self.bucket_data_names)
+            # data_bucket_len = len(data_bucket) + 1
+            data_fields = [field for field in self.memory['data'].columns if data_bucket in field]
+
+            crud = self.memory['telemetry_description_crud']
+            data = pd.DataFrame([crud.read(data_bucket, field) for field in data_fields], index=data_fields)
+
+            text_mode = st.checkbox("Text mode", key=f"{self.name} text mode")
+            text_config = st.column_config.TextColumn(width='small')
+            select_box_config = st.column_config.SelectboxColumn(options=self.units, default="None", width='small')
+            unit_config = text_config if text_mode else select_box_config
+            edited_data = st.data_editor(
+                data, use_container_width=True,
+                key=f"{self.name} data editor",
+                column_config={
+                    "unit": unit_config,
+                    "description": st.column_config.TextColumn(help="Enter a description", width='large'),
+                },
+                column_order=["unit", "description"]
+            )
+
+            if st.button("Save", key=f"{self.name} save button"):
+                for field, row in edited_data.iterrows():
+                    crud.create(field, row['unit'], row['description'])
+                st.success("Data saved")
+
+
+
+
+
