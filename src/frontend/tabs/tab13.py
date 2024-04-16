0853dbc0c8a01e4bc91e021a6af5f89459a3d3e5
chahoud <sebastien.chahoud@epfl.ch>
2024-04-16 15:47:22 +0200

    improvments

--- src/frontend/tabs/tab13.py
+++ src/frontend/tabs/tab13.py
@@ -2,9 +2,12 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from matplotlib import pyplot as plt
+from stqdm import stqdm
 
 from src.backend.state_estimation.config.state_estimation_param import SE_param
 from src.backend.state_estimation.kalman_filters.estimation_transformation import estimate_normal_forces
+from src.backend.state_estimation.measurments.sensors import Sensors, get_sensors_from_data
+from src.backend.state_estimation.state_estimator_app import StateEstimatorApp
 from src.frontend.plotting.plotting import plot_data
 from src.frontend.tabs import Tab
 from src.backend.state_estimation.config.vehicle_params import VehicleParams
@@ -53,63 +56,111 @@ class Tab13(Tab):
         self.slip_cols100 = [f'sensors_s_{wheel}_est_100' for wheel in VehicleParams.wheel_names]
         self.slip_cols1000 = [f'sensors_s_{wheel}_est_1000' for wheel in VehicleParams.wheel_names]
 
+    def create_new_feature(self):
+        data = self.memory['data']
+
+        # Create steering wheel angle and steering rad
+        data[self.steering_col + "_rad"] = data[self.steering_col].map(np.deg2rad)
+        data[self.delta_wheel_angle_cols] = data[[self.steering_col]].apply(
+            lambda x: measure_delta_wheel_angle(x[0]), axis=1, result_type='expand')
+
+        # Create slip10 slip100 and slip1000
+        data[self.slip_cols10] = data[self.slip_cols].copy() * 10
+        data[self.slip_cols100] = data[self.slip_cols].copy() * 100
+        data[self.slip_cols1000] = data[self.slip_cols].copy() * 1000
+
+        # Create wheel speeds and longitudinal velocity
+        data[self.wheel_speeds_cols] = data[self.motor_speeds_cols].apply(
+            lambda x: measure_wheel_speeds(x) * VehicleParams.Rw, axis=1, result_type='expand')
+        data[self.wheel_speeds_est_cols] = data[
+            SE_param.estimated_states_names + self.delta_wheel_angle_cols].apply(
+            lambda x: estimate_wheel_speeds(x[:9], x[9:]) * VehicleParams.Rw, axis=1, result_type='expand'
+        )
+        data[self.vl_cols] = data[SE_param.estimated_states_names + self.delta_wheel_angle_cols].apply(
+            lambda x: estimate_longitudinal_velocities(x[:9], x[9:]), axis=1, result_type='expand'
+        )
+
+        # Create wheel acceleration and Reset wheel acceleration
+        for i in range(30):
+            measure_wheel_acceleration(wheel_speeds=np.array([0, 0, 0, 0], dtype=float))
+        data[self.wheel_acceleration_cols] = np.array([
+            measure_wheel_acceleration(wheel_speeds=wheel_speeds)
+            for wheel_speeds in data[self.wheel_speeds_cols].values
+        ])
+
+        # Create Normal Forces
+        data[self.normal_forces_cols] = data[SE_param.estimated_states_names].apply(estimate_normal_forces,
+                                                                                    axis=1,
+                                                                                    result_type='expand')
+
+        # Create Longitudinal Forces
+        data[self.longitudinal_forces_cols] = data[
+            self.motor_torques_cols + self.brake_pressure_cols + self.wheel_speeds_cols + self.wheel_acceleration_cols].apply(
+            lambda x: measure_tire_longitudinal_forces(x[:4], x[4:8], x[8:12], x[12:]), axis=1,
+            result_type='expand'
+        )
+        data[self.longitudinal_forces_est_cols] = data[SE_param.estimated_states_names].apply(
+            lambda x: estimate_longitudinal_tire_forces(x, use_traction_ellipse=False), axis=1,
+            result_type='expand'
+        )
+
+        self.memory['data'] = data.copy()
+
+    def compute_state_estimator(self):
+
+        data = self.memory['data']
+        samples = (data.index[0], data.index[-1])
+
+        sensors_list: list[Sensors] = get_sensors_from_data(data.loc[samples[0]:samples[1]])
+        estimator_app = StateEstimatorApp(independent_updates=False)
+        estimations = [np.zeros(SE_param.dim_x) for _ in sensors_list]
+        estimations_cov = [np.zeros(SE_param.dim_x) for _ in sensors_list]
+        for i, sensors in stqdm(enumerate(sensors_list), total=len(sensors_list)):
+            state, cov = estimator_app.run(sensors)
+            estimations[i] = state
+            estimations_cov[i] = cov
+
+        # Update the data
+        columns = SE_param.estimated_states_names
+        data.loc[samples[0]: samples[1], columns] = np.array(estimations)
+        self.memory['data'] = data.copy()
+
+        # Update the data_cov
+        index = data.loc[samples[0]: samples[1]].index
+        data_cov = pd.DataFrame(estimations_cov, index=index, columns=columns)
+        self.memory['data_cov'] = data_cov.copy()
+        st.balloons()
+
     def build(self, session_creator) -> bool:
 
         st.header(self.description)
         datetime_range = session_creator.r2d_session_selector(st.session_state.sessions,
                                                               key=f"{self.name} session selector")
-        if st.button("Fetch this session", key=f"{self.name} fetch data button"):
+
+        cols = st.columns(6)
+        if cols[0].button("Fetch this session", key=f"{self.name} fetch data button"):
             data = session_creator.fetch_data(datetime_range, verify_ssl=st.session_state.verify_ssl)
             data.index = (data.index - data.index[0]).total_seconds()
+            self.memory['data'] = data
 
-            with st.spinner("Creating new features"):
+        if len(self.memory['data']) > 0:
+            cols[1].success("Data fetched")
 
-                # Create steering wheel angle and steering rad
-                data[self.steering_col + "_rad"] = data[self.steering_col].map(np.deg2rad)
-                data[self.delta_wheel_angle_cols] = data[[self.steering_col]].apply(
-                    lambda x: measure_delta_wheel_angle(x[0]), axis=1, result_type='expand')
+        if cols[2].button("Compute State Estimation", key=f"{self.name} compute state estimation"):
+            with cols[2].status("Computing state estimation..."):
+                self.compute_state_estimator()
 
-                # Create slip10 slip100 and slip1000
-                data[self.slip_cols10] = data[self.slip_cols].copy() * 10
-                data[self.slip_cols100] = data[self.slip_cols].copy() * 100
-                data[self.slip_cols1000] = data[self.slip_cols].copy() * 1000
+        if len(self.memory['data_cov']) > 0:
+            cols[3].success("Computed")
 
-                # Create wheel speeds and longitudinal velocity
-                data[self.wheel_speeds_cols] = data[self.motor_speeds_cols].apply(
-                    lambda x: measure_wheel_speeds(x) * VehicleParams.Rw, axis=1, result_type='expand')
-                data[self.wheel_speeds_est_cols] = data[
-                    SE_param.estimated_states_names + self.delta_wheel_angle_cols].apply(
-                    lambda x: estimate_wheel_speeds(x[:9], x[9:]) * VehicleParams.Rw, axis=1, result_type='expand'
-                )
-                data[self.vl_cols] = data[SE_param.estimated_states_names + self.delta_wheel_angle_cols].apply(
-                    lambda x: estimate_longitudinal_velocities(x[:9], x[9:]), axis=1, result_type='expand'
-                )
+        if cols[4].button("Create new features", key=f"{self.name} create new features"):
+            with cols[4].status("Creating new features"):
+                self.create_new_feature()
 
-                # Create wheel acceleration and Reset wheel acceleration
-                for i in range(30):
-                    measure_wheel_acceleration(wheel_speeds=np.array([0, 0, 0, 0], dtype=float))
-                data[self.wheel_acceleration_cols] = np.array([
-                    measure_wheel_acceleration(wheel_speeds=wheel_speeds)
-                    for wheel_speeds in data[self.wheel_speeds_cols].values
-                ])
+        if self.normal_forces_cols[0] in self.memory['data'].columns:
+            cols[5].success("Created")
 
-                # Create Normal Forces
-                data[self.normal_forces_cols] = data[SE_param.estimated_states_names].apply(estimate_normal_forces,
-                                                                                            axis=1,
-                                                                                            result_type='expand')
-
-                # Create Longitudinal Forces
-                data[self.longitudinal_forces_cols] = data[
-                    self.motor_torques_cols + self.brake_pressure_cols + self.wheel_speeds_cols + self.wheel_acceleration_cols].apply(
-                    lambda x: measure_tire_longitudinal_forces(x[:4], x[4:8], x[8:12], x[12:]), axis=1,
-                    result_type='expand'
-                )
-                data[self.longitudinal_forces_est_cols] = data[SE_param.estimated_states_names].apply(
-                    lambda x: estimate_longitudinal_tire_forces(x, use_traction_ellipse=False), axis=1,
-                    result_type='expand'
-                )
-
-                self.memory['data'] = data
+        st.divider()
 
         if len(self.memory['data']) > 0:
             data = self.memory['data']
@@ -126,76 +177,90 @@ class Tab13(Tab):
 
             with st.container(border=True):
                 mode_int = data[self.knob_mode].iloc[0]
-                ellapsed_time = data.index[-1] - data.index[0]
+                elapsed_time = data.index[-1] - data.index[0]
                 arg_max_accx = data['sensors_aXEst'].rolling(10).mean().idxmax()
                 max_accx = data['sensors_aXEst'].rolling(10).mean().max()
                 arg_max_vx = data['sensors_vXEst'].rolling(10).mean().idxmax()
                 max_vx = data['sensors_vXEst'].rolling(10).mean().max()
+                mean_accx = data['sensors_aXEst'].mean()
 
-                cols = st.columns([2, 2, 3, 3])
+                # Compute distance from velocity
+                data['sensors_vXEst_integrated'] = data['sensors_vXEst'].cumsum() * 0.01
+                distance = data['sensors_vXEst_integrated'].iloc[-1]
 
                 # Show metrics
+                cols = st.columns([2, 2, 3, 3, 3, 3])
                 cols[0].metric("Mode", VehicleParams.ControlMode[mode_int])
-                cols[1].metric("Time", f"{ellapsed_time:.2f} s")
-                cols[2].metric("Max AccX", f"{max_accx:.2f} m/s²", f"At {arg_max_accx:.2f} s", delta_color="off")
-                cols[3].metric("Max VX", f"{max_vx:.2f} m/s", f"At {arg_max_vx:.2f} s", delta_color="off")
+                cols[1].metric("Time", f"{elapsed_time:.2f} s")
+                cols[2].metric("Mean AccX", f"{mean_accx:.2f} m/s²")
+                cols[3].metric("Max AccX", f"{max_accx:.2f} m/s²", f"At {arg_max_accx:.2f} s", delta_color="off")
+                cols[4].metric("Max VX", f"{max_vx:.2f} m/s", f"At {arg_max_vx:.2f} s", delta_color="off")
+                cols[5].metric("Distance", f"{distance:.2f} m")
 
             # PLot acceleration and speed
             with st.expander("Acceleration and Speed"):
-                data['v_acc_integrated'] = data['sensors_aXEst'].cumsum() * 0.01
+                data['v_acc_integrated'] = data['sensors_accX'].cumsum() * 0.01
                 plot_data(data=data, tab_name=self.name + "AS", title="Overview",
                           default_columns=self.acc_cols + self.speed_cols + ['v_acc_integrated'])
 
             # Plot wheel speeds
             with st.expander("Wheel Speeds"):
-                plot_data(data=data, tab_name=self.name + "WS", title="Wheel Speeds",
+                if st.toggle("Show wheel speeds", key=f"{self.name} show wheel speeds"):
+                    plot_data(data=data, tab_name=self.name + "WS", title="Wheel Speeds",
                           default_columns=self.wheel_speeds_cols + self.speed_cols[:1] + ['v_acc_integrated'])
 
             # Plot the wheel slip
             with st.expander("Wheel Slip"):
-                plot_data(data=data, tab_name=self.name + "Slip", title="Slip Ratios", default_columns=self.slip_cols)
+                if st.toggle("Show wheel slip", key=f"{self.name} show wheel slip"):
+                    plot_data(data=data, tab_name=self.name + "Slip", title="Slip Ratios", default_columns=self.slip_cols)
 
             # Sanity check: plot the wheel speeds estimation
             with st.expander("Wheel Speeds Estimation"):
-                fig, ax = plt.subplots(2, 2, figsize=(15, 10))
-                for i, wheel in enumerate(VehicleParams.wheel_names):
-                    cols = [self.wheel_speeds_cols[i], self.wheel_speeds_est_cols[i], self.vl_cols[i]]
-                    data[cols].plot(ax=ax[i // 2, i % 2], title=f"Wheel {wheel} speed")
-                plt.tight_layout()
-                st.pyplot(fig)
+                if st.toggle("Show wheel speeds estimation", key=f"{self.name} show wheel speeds estimation"):
+                    fig, ax = plt.subplots(2, 2, figsize=(15, 10))
+                    for i, wheel in enumerate(VehicleParams.wheel_names):
+                        cols = [self.wheel_speeds_cols[i], self.wheel_speeds_est_cols[i], self.vl_cols[i]]
+                        data[cols].plot(ax=ax[i // 2, i % 2], title=f"Wheel {wheel} speed")
+                    plt.tight_layout()
+                    st.pyplot(fig)
 
             # Plot the normal forces
             with st.expander("Normal Forces"):
-                plot_data(data=data, tab_name=self.name + "NF", title="Normal Forces",
-                          default_columns=self.normal_forces_cols)
+                if st.toggle("Show normal forces", key=f"{self.name} show normal forces"):
+                    plot_data(data=data, tab_name=self.name + "NF", title="Normal Forces",
+                              default_columns=self.normal_forces_cols)
 
             # PLot wheel accelerations
             with st.expander("Wheel Accelerations"):
-                plot_data(data=data, tab_name=self.name + "WA", title="Wheel Accelerations",
-                          default_columns=self.wheel_acceleration_cols)
+                if st.toggle("Show wheel accelerations", key=f"{self.name} show wheel accelerations"):
+                    plot_data(data=data, tab_name=self.name + "WA", title="Wheel Accelerations",
+                              default_columns=self.wheel_acceleration_cols)
 
             # Plot the longitudinal forces
             with st.expander("Longitudinal Forces"):
-                fig, ax = plt.subplots(2, 2, figsize=(15, 10))
-                for i, wheel in enumerate(VehicleParams.wheel_names):
-                    cols = [self.longitudinal_forces_cols[i], self.longitudinal_forces_est_cols[i],
-                            self.slip_cols1000[i]]
-                    data[cols].plot(ax=ax[i // 2, i % 2], title=f"Wheel {wheel} longitudinal force")
-                plt.tight_layout()
-                st.pyplot(fig)
+                if st.toggle("Show longitudinal forces", key=f"{self.name} show longitudinal forces"):
+                    fig, ax = plt.subplots(2, 2, figsize=(15, 10))
+                    for i, wheel in enumerate(VehicleParams.wheel_names):
+                        cols = [self.longitudinal_forces_cols[i], self.longitudinal_forces_est_cols[i],
+                                self.slip_cols1000[i]]
+                        data[cols].plot(ax=ax[i // 2, i % 2], title=f"Wheel {wheel} longitudinal force")
+                    plt.tight_layout()
+                    st.pyplot(fig)
 
             # Plot wheel torques
             with st.expander("Wheel MIN/MAX Torques"):
-                add_slips = st.checkbox("Add slip ratios", key=f"{self.name} add slips")
-                fig, ax = plt.subplots(2, 2, figsize=(15, 10))
-                for i, wheel in enumerate(VehicleParams.wheel_names):
-                    cols = [self.motor_torques_cols[i], self.max_motor_torques_cols[i], self.min_motor_torques_cols[i]]
-                    if add_slips:
-                        cols += [self.slip_cols1000[i]]
-                    data[cols].plot(ax=ax[i // 2, i % 2], title=f"Wheel {wheel} torques")
-                plt.tight_layout()
-                st.pyplot(fig)
+                if st.toggle("Show wheel torques", key=f"{self.name} show wheel min/max torques"):
+                    add_slips = st.checkbox("Add slip ratios", key=f"{self.name} add slips")
+                    fig, ax = plt.subplots(2, 2, figsize=(15, 10))
+                    for i, wheel in enumerate(VehicleParams.wheel_names):
+                        cols = [self.motor_torques_cols[i], self.max_motor_torques_cols[i], self.min_motor_torques_cols[i]]
+                        if add_slips:
+                            cols += [self.slip_cols1000[i]]
+                        data[cols].plot(ax=ax[i // 2, i % 2], title=f"Wheel {wheel} torques")
+                    plt.tight_layout()
+                    st.pyplot(fig)
 
             with st.expander("Wheel torques"):
-                plot_data(data=data, tab_name=self.name + "WT", title="Wheel Torques",
-                          default_columns=self.motor_torques_cols)
+                if st.toggle("Show wheel torques", key=f"{self.name} show wheel torques"):
+                    plot_data(data=data, tab_name=self.name + "WT", title="Wheel Torques",
+                              default_columns=self.motor_torques_cols)
